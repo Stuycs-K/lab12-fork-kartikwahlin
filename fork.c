7ef133d1db3600f2c8e0f9f14418ec4104c44df6
Kartik Wahlin <kwahlin50@stuy.edu>
2024-11-26 14:34:24 -0500

    updated to fix branching

--- fork.c
+++ fork.c
@@ -1,11 +1,41 @@
 #include <wait.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <time.h>
 
 int main(){
+  pid_t p;
+  p = fork();
+  if(p<0){
+    perror("fork fail");//output to stderr instead of stdout
+    exit(1);
+  } else if ( p == 0){
+    srand(time(NULL));
+    int rando = rand();
+    int myPid = getpid();
+    printf("PID: %d %d\n", myPid,rando);
+    sleep(rando);
+    printf("%d FINISHED",myPid);
+  }else{
+      printf("Hello from Parent!\n");
+      pid_t p2;
+      p2 = fork();
+      if(p2<0){
+        perror("fork fail");//output to stderr instead of stdout
+        exit(1);
+      } else if ( p2 == 0){
+        srand(time(NULL)+1);
+        int rando = rand();
+        int myPid = getpid();
+        printf("PID: %d %d\n", myPid,rando);
+        sleep(rando);
+        printf("%d FINISHED",myPid);
+      }else{
+          printf("Hello from Parent2!\n");
+      }
+  }
 
 
-  int f1= fork();
-  int f2 = fork();
-  printf("%d",f1);
-  printf("%d",f2);
 }
